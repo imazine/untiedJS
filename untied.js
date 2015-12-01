2458e4a0b9acbca54232dcdfbd3b2ebf154815c8
iMaZiNe <imazine@me.com>
2015-12-01 17:00:31 +0900

    Fix indentations

--- untied.js
+++ untied.js
@@ -1,22 +1,20 @@
 /**
- * UntiedJS 
+ * UntiedJS
  */
 
 var UntiedJS = {};
 
 (function() {
-
   var
-  eventListenerMap = {},
-  isReady = false;
+    eventListenerMap = {},
+    isReady = false;
 
   // 추적하고자 하는 이벤트 추가
   function watchEvent() {
-
     var
-    argsLength = arguments.length,
-    eventName,
-    i;
+      argsLength = arguments.length,
+      eventName,
+      i;
 
     for (i = 0; i < argsLength; i += 1) {
       eventName = arguments[i];
@@ -24,7 +22,6 @@ var UntiedJS = {};
       if (eventListenerMap[eventName] === undefined) {
         eventListenerMap[eventName] = [];
       }
-
       if (isReady === true) {
         attachEvent(eventName);
       }
@@ -33,63 +30,51 @@ var UntiedJS = {};
 
   // 이벤트 제거
   function unwatchEvent() {
-
     var
-    eventName,
-    eventListeners,
-    eventListener;
+      eventName,
+      eventListeners,
+      eventListener;
 
-        for (eventName in arguments) {
-            eventListeners = eventListenerMap[eventName];
-            
-            for (eventListener in eventListeners) {
-                detachEvent(eventName, eventListeners[eventListener]);
-            }
-        }
+    for (eventName in arguments) {
+      eventListeners = eventListenerMap[eventName];
+      for (eventListener in eventListeners) {
+          detachEvent(eventName, eventListeners[eventListener]);
+      }
+    }
   }
 
   // 이벤트 리스너 등록
   function attachEvent(eventName) {
-        
     var eventListeners = eventListenerMap[eventName] || [];
-
     var eventListener = function(e) {
-
       var
-      target = e.target,
-      targets,
-      eventName = e.type,
-      funcName,
-      funcNameSplits,
-      funcNameSplitsLength,
-      funcNameSplit,
-      func = window,
-      i;
+        target = e.target,
+        targets,
+        eventName = e.type,
+        funcName,
+        funcNameSplits,
+        funcNameSplitsLength,
+        funcNameSplit,
+        func = window,
+        i;
 
       if (target === window) {
         targets = document.querySelectorAll('[data-' + eventName + ']');
-
         for (i = 0; i < targets.length; i += 1) {
           eventListener({
             target : targets[i],
             type : eventName
           });
         }
-      }
-
-      else {
-
+      } else {
         funcName = target.getAttribute('data-' + eventName);
-
         if (funcName !== null) {
           funcNameSplits = funcName.split('.');
           funcNameSplitsLength = funcNameSplits.length;
-
           for (i = 0; i < funcNameSplitsLength; i += 1) {
             funcNameSplit = funcNameSplits[i];
             func = func[funcNameSplit];
           }
-
           func(e);
         }
       }
@@ -100,7 +85,6 @@ var UntiedJS = {};
     } else {
       document.body.addEventListener(eventName, eventListener, false);
     }
-
     eventListeners.push(eventListener);
   }
 
@@ -112,7 +96,6 @@ var UntiedJS = {};
   // 모든 문서 로딩이 끝난 후
   window.addEventListener('load', function() {
     var eventNames = Object.getOwnPropertyNames(eventListenerMap), eventName;
-        
     for (eventName in eventNames) {
       attachEvent(eventNames[eventName]);
     }
@@ -122,5 +105,4 @@ var UntiedJS = {};
 
   UntiedJS.watchEvent = watchEvent;
   UntiedJS.unwatchEvent = unwatchEvent;
-
 })();
